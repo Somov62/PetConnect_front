7618ef0e150eecb43e45091e29802bfba2b36d54
somov <somov@pulsarm.ru>
2024-08-21 23:35:34 +0300

    first commit

---
+++ petconnect/src/App.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Text } from '@chakra-ui/react';
+import './App.css'
+
+function App() {
+
+  return <div className="h-screen flex justify-center items-center bg-slate-300">
+      <Card className='h-1/5 b'>
+        <CardBody>
+          <Text>asajsd</Text>
+        </CardBody>
+      </Card>
+    </div>
+}
+
+
+export default App
